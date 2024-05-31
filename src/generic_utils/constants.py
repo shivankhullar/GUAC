4b11ed05890d19b013f199a041266e4756e48bf2
shivankhullar <shivankhullar@gmail.com>
2024-05-31 00:55:25 -0400

    store all constants in cgs units

---
+++ src/generic_utils/constants.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# All units are in cgs
+AU = 1.496e13
+pc = 3.086e18
+Mpc = 1e6*pc
+kpc = 1e3*pc
+Gpc = 1e9*pc
+
+c = 3e10
+yr = 24*365.25*3600
+Myr = yr*10**6
+Gyr = yr*1e9
+ly = pc/3.26156
+
+Msun = 1.989e33
+G = 6.67e-8
+k_B = 1.38e-16
+m_p = 1.67e-24
+m_e = 9.1e-28
+
+h = 6.626e-27
+e = 4.8e-10   # in e.s.u. -> cgs units for Coulomb
+km = 1e5
+cm = 1
+m = 1e2
+
+eV = 1e-12
+MeV = 1e6*eV
+GeV = 1e9*eV
+keV = 1e3*eV
+J = 1e7
+
+Lsun = 3.846e33   #erg/s
