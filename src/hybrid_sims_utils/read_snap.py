b2e1c8920a8815d4e3f4afed2a671d426719531c
Shivan Khullar <shivankhullar@gmail.com>
2025-10-14 19:18:28 -0400

    function to convert to physical units

--- src/hybrid_sims_utils/read_snap.py
+++ src/hybrid_sims_utils/read_snap.py
@@ -80,3 +80,34 @@ def get_snap_data_hybrid(sim, sim_path, snap, snapshot_suffix='', snapdir=True, 
     F.close()
 
     return pdata, stardata, fire_stardata, refine_data, snapname
+
+
+
+
+def convert_units_to_physical(pdata, stardata, fire_stardata):
+    if pdata:
+        a = pdata['Time']
+        h = pdata['HubbleParam']
+        hinv = 1.0 / h
+        rconv = a * hinv 
+
+    data_dicts = [pdata, stardata, fire_stardata]
+    for data_dict in data_dicts: 
+        for key in data_dict.keys():
+            if key == "Coordinates" or key == "SmoothingLength":
+                data_dict[key] = data_dict[key] * rconv
+            elif key == "Velocities":
+                data_dict[key] = data_dict[key] * np.sqrt(a)
+            elif key == "Masses" or key == "BH_Mass" or key == "CosmicRayEnergy" or key == "PhotonEnergy":
+                data_dict[key] = data_dict[key] * hinv
+            elif key == "Density" or key == "Pressure":
+                data_dict[key] = data_dict[key] * hinv / (rconv**3)
+            elif key == "Acceleration" or key == "HydroAcceleration" or key == "VelocityGradient" or key == "RadiativeAcceleration":
+                data_dict[key] = data_dict[key] * h
+            elif key == "DensityGradient":
+                data_dict[key] = data_dict[key] * hinv / (hinv**4)
+            else:
+                pass
+        
+
+    return pdata, stardata, fire_stardata
